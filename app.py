0f50524070a330d5c5bc008c6d657c6be8dba537
ik_CYB <ik_cyb@TX220710.h015110.net>
2022-10-11 12:34:10 +0200

    login funcional

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    for user in users:
+        if user.id == int(user_id):
+            return user
+    return None
+
 
 @app.route('/')
 def index():
